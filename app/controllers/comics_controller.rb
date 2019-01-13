83df3f248706f3dbbcae380dfa9e832d362ffc04
Uncanny Forest <git@uncannyforest.com>
2019-01-12 22:52:40 -0700

    Added nifty print of url id :)

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,4 +1,5 @@
 class ComicsController < ApplicationController
   def show
+    @id = params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'comics/show'
+  get 'comics/:id' => 'comics#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'comics#show'
 end
