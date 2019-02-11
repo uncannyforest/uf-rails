85be7ed2eede71a2ceebf1e12758e17d33cdf28b
Uncanny Forest <git@uncannyforest.com>
2019-02-10 20:39:44 -0700

    add a line

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,4 +1,6 @@
 class ComicsController < ApplicationController
+  @@json_url = "https://drive.google.com/uc?authuser=0&id=1bcSqSZFwnBmnDXHbiUU3IkbHk99ZlVbT&export=download"
+
   def show
     @id = params[:id]
   end
