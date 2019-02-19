03a6474e5708a1953f1481adb532f4b0af8dd9cf
Uncanny Forest <git@uncannyforest.com>
2019-02-18 17:55:59 -0700

    Lotta tweaks, mostly visual.  Site has nice text header now

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -3,6 +3,7 @@ class ComicsController < ApplicationController
     @id = params[:id].to_i
     @panels = PanelList.new @id
     @exists_prev = @id > 0
+    @link_first = "/c/0"
     @link_prev = "/c/#{@id-1}"
     @exists_next = @id < UF_RECORD.numComics - 1
     @link_next = "/c/#{@id+1}"
