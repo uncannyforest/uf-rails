2ad0269d5b0acaba7bb707a2aed7274c467eaedf
Uncanny Forest <git@uncannyforest.com>
2019-02-21 21:39:41 -0700

    No spoilers\!

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -5,7 +5,7 @@ class ComicsController < ApplicationController
     @exists_prev = @id > 0
     @link_first = "/c/0"
     @link_prev = "/c/#{@id-1}"
-    @exists_next = @id < UF_RECORD.numComics - 1
+    @exists_next = @id < UF_RECORD.num_comics - 1
     @link_next = "/c/#{@id+1}"
   end
 
--- app/models/uf_record.rb
+++ app/models/uf_record.rb
@@ -2,17 +2,35 @@ require 'yaml'
 
 class UfRecord
 
+  @@time_zone = "Mountain Time (US & Canada)"
+
+  attr_reader :num_comics
+
   def initialize
     @data = (YAML.load(File.read(config = File.join(Rails.root, 'config', 'uncannyforest.yaml'))))
     @data.deep_symbolize_keys!
+    init_num_comics
   end
 
   def comics
     @data[:comics]
   end
 
-  def numComics
-    @data[:comics].length
+  def init_num_comics
+    max_comic = @data[:comics].length - 1
+    while spoilers max_comic
+      max_comic = max_comic - 1
+    end
+    @num_comics = max_comic + 1
+  end
+
+  def spoilers comic
+    comic_date = @data[:comics][comic][:date]
+    if comic_date.nil?
+      return false
+    end
+    today = Time.now.in_time_zone(@@time_zone).to_date
+    comic_date > today
   end
 
 end
