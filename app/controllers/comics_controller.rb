ecb0a9a147a673e196ead7c9c5ae6f19cecbecf1
Uncanny Forest <git@uncannyforest.com>
2019-02-26 23:36:38 -0700

    Change image URL to top level

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -3,11 +3,11 @@ class ComicsController < ApplicationController
     @id = params[:id].to_i
     @panels = Panel.list @id, UF_RECORD.comics[@id][:layout]
     @exists_prev = @id > 0
-    @link_first = "/c/0"
-    @link_prev = "/c/#{@id-1}"
+    @link_first = "/0"
+    @link_prev = "/#{@id-1}"
     @exists_next = @id < UF_RECORD.num_comics - 1
-    @link_next = "/c/#{@id+1}"
-    @link_last = "/c/#{UF_RECORD.num_comics - 1}"
+    @link_next = "/#{@id+1}"
+    @link_last = "/#{UF_RECORD.num_comics - 1}"
   end
 
 end
