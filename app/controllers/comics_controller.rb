1193d9782433dd4a47075eb1f42264f92799b0d2
Uncanny Forest <git@uncannyforest.com>
2019-03-04 23:55:19 -0700

    Separate out main layout from comics view (and customize title\!)

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,6 +1,9 @@
 class ComicsController < ApplicationController
+  TITLE_PREFIX = "Uncanny Forest — "
+
   def show
     @id = params[:id].to_i
+    @title = TITLE_PREFIX + UF_RECORD.comics[@id][:title]
     @panels = Panel.list @id, UF_RECORD.comics[@id][:layout]
     @exists_prev = @id > 0
     @link_first = "/0"
@@ -8,6 +11,7 @@ class ComicsController < ApplicationController
     @exists_next = @id < UF_RECORD.num_comics - 1
     @link_next = "/#{@id+1}"
     @link_last = "/#{UF_RECORD.num_comics - 1}"
+    @analyze = true
   end
 
 end
