ef3995694b1f509f12145ac198e852446e27ac79
Uncanny Forest <git@uncannyforest.com>
2019-03-24 19:34:03 -0600

    Add cookie functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
+require 'prefs'
+
 class ApplicationController < ActionController::Base
+  before_action :process_prefs, :prep
+
+  def process_prefs
+    @prefs = Prefs.new(cookies)
+
+    request.query_parameters.each do |key, value|
+      if @prefs.is_valid?(key, value)
+        @prefs.set(key, value)
+      end
+    end
+  end
+
+  def prep
+    @dont_track = @prefs.get(:dont_track)
+  end
+
 end
---
+++ lib/prefs.rb
@@ -0,0 +1,39 @@
+class Prefs
+  # first in array is the default
+  @@prefs = {
+    dont_track: [:false, :true]
+  }.with_indifferent_access
+
+  def initialize(cookies)
+    @cookies = cookies
+  end
+
+  def set(key, value)
+    if @@prefs[key][0] == value.to_sym # default
+      @cookies.delete key
+    else
+      @cookies[key] = {value: value, expires: 1.year}
+    end
+  end
+
+  def get(key)
+    if @cookies[key].nil? # default
+      value = @@prefs[key][0]
+    else
+      value = @cookies[key].to_sym
+    end
+
+    if value == :true
+      true
+    elsif value == :false
+      false
+    else
+      value
+    end
+  end
+
+  def is_valid?(key, value)
+    @@prefs.has_key?(key) && @@prefs[key].include?(value.to_sym)
+  end
+
+end
