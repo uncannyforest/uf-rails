1b26e6ab2c645fc2075d929fdb4d54a534846148
Uncanny Forest <git@uncannyforest.com>
2020-01-29 21:57:13 -0500

    Replace comic navigation with images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,9 +6,9 @@ module ApplicationHelper
 
   def section(link)
     if (@section == link)
-      'page-' + link.to_s + '-sel-2x.png'
+      "page-#{link.to_s}-sel-2x.png"
     else
-      'page-' + link.to_s + '-2x.png'
+      "page-#{link.to_s}-2x.png"
     end
   end
 end
--- app/helpers/comics_helper.rb
+++ app/helpers/comics_helper.rb
@@ -1,2 +1,10 @@
 module ComicsHelper
+
+  def nav_asset(asset, condition = false)
+    if (condition)
+      "nav-#{asset.to_s}-sel-2x.png"
+    else
+      "nav-#{asset.to_s}-2x.png"
+    end
+  end
 end
